b1c6bc19b3ff1d47956b6df8b7efd0e867c4e602
Dev Kumar <devutmani.2@gmail.com>
2025-02-03 23:22:47 +0500

    Task4.cpp

---
+++ Lab 2/Task4.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+void add(double num1, double num2, double* result);
+void subtract(double num1, double num2, double* result);
+void multiply(double num1, double num2, double* result);
+void divide(double num1, double num2, double* result);
+
+int main()
+{
+    double num1;
+    double num2;
+    double result;
+    int operation;
+
+    cout<<"\n<----------------------------------->"<<endl;
+    cout<<"\nEnter number 1: ";
+    cin>>num1;
+
+    cout<<"<----------------------------------->"<<endl;
+    cout<<"Enter number 2: ";
+    cin>>num2;
+
+    cout<<"<----------------------------------->"<<endl;
+    cout<<"\nChoose Operation: "<<endl;
+    cout<<"1. Addition"<<endl;
+    cout<<"2. Subtraction"<<endl;
+    cout<<"3. Multiplication"<<endl;
+    cout<<"4. Division"<<endl;
+    cout<<"<----------------------------------->"<<endl;
+
+    cout<<"\nWhat operation you want to perform: ";
+    cin>>operation;
+    cout<<"<----------------------------------->"<<endl;
+
+    switch (operation)
+    {
+    case 1:
+        add(num1, num2, &result);
+        cout<<"<----------------------------------->"<<endl;
+        cout<<"\nSum of "<<num1<<" and "<<num2<<" is: "<<result<<endl;
+        cout<<"<----------------------------------->"<<endl;
+        break;
+
+    case 2:
+        subtract(num1, num2, &result);
+        cout<<"<----------------------------------->"<<endl;
+        cout<<"\nSubtraction of "<<num2<<" from "<<num1<<" is: "<<result<<endl;
+        cout<<"<----------------------------------->"<<endl;
+        break;
+
+    case 3:
+        multiply(num1, num2, &result);
+        cout<<"<----------------------------------->"<<endl;
+        cout<<"\nMultiplication of "<<num1<<" and "<<num2<<" is: "<<result<<endl;
+        cout<<"<----------------------------------->"<<endl;
+        break;
+
+    case 4:
+        divide(num1, num2, &result);
+        cout<<"<----------------------------------->"<<endl;
+        cout<<"\nDivision of "<<num2<<" from "<<num1<<" is: "<<result<<endl;
+        cout<<"<----------------------------------->"<<endl;
+        break;
+    
+    default:
+        break;
+    }
+}
+
+void add(double num1, double num2, double* result)
+{
+    *result = num1 + num2;
+}
+
+void subtract(double num1, double num2, double* result)
+{
+    *result = num1 - num2;
+}
+
+void multiply(double num1, double num2, double* result)
+{
+    *result = num1*num2;
+}
+
+void divide(double num1, double num2, double* result)
+{
+    *result = num1/num2;
+}
