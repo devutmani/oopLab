9a09371bbe0dc83b65ed512f1554955d2fe6cdb1
Dev Kumar <devutmani.2@gmail.com>
2025-02-18 00:48:55 +0500

    Task1.cpp

--- Lab 3/Task1.cpp
+++ Lab 3/Task1.cpp
@@ -1,1 +1,62 @@
-// Name Dev Kumar
+#include <iostream>
+using namespace std;
+
+class BoardMarker
+{
+private:
+    bool refillable;
+    bool inkStatus;
+
+public:
+    string company;
+    string color;
+
+    void setAttributes(string comp, string col)
+    {
+        company = comp;
+        color = col;
+        refillable = true;
+        inkStatus = true;
+    }
+
+    void write()
+    {
+        if (inkStatus == false)
+        {
+            cout<<"\nWriting is not possible!."<<endl;
+            return;
+        }
+        cout<<"\nWriting..."<<endl;
+        inkStatus = false;
+        return;
+    }
+
+    void refill()
+    {
+        if (refillable == true && inkStatus == false)
+        {
+            inkStatus = true;
+            return;
+        }
+        cout<<"This marker is not refillable!..."<<endl;
+        return;
+    }
+
+    void display()
+    {
+        cout<<"Company: "<<company<<endl;
+        cout<<"Color is: "<<color<<endl;
+    }
+};
+
+int main()
+{
+    BoardMarker m1;
+    m1.setAttributes("Doller", "Red");
+    m1.write();
+    m1.refill();
+
+    cout<<endl;
+    m1.display();
+    return 0;
+}
